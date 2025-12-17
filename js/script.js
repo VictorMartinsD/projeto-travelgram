b757a905053c6f1a0b97dafc50825247e923da65
Victor Martins Dias <victormartinsjob@gmail.com>
2025-12-17 18:18:15 -0300

    refactor: atualizar fontes, remover pastas e corrigir caminhos globais

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function changeEndPoint (caminho) {
-  window.location.href = caminho
-}
-
-// Fallback em JavaScript caso o meta tag falhe em algum navegador antigo
-window.location.href = "./static/index.html";
